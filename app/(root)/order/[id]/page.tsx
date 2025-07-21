2e4ba7eb95363b6e01e6dc28509de02df7638f9f
Carlos1016 <riveracarlos403@gmail.com>
2025-07-21 10:50:02 -0400

    add commit

--- app/(root)/order/[id]/page.tsx
+++ app/(root)/order/[id]/page.tsx
@@ -1,46 +1,51 @@
-import { Metadata } from 'next';
-import { getOrderById } from '@/lib/actions/order.actions';
-import { notFound, redirect } from 'next/navigation';
-import OrderDetailsTable from './order-details-table';
-import { ShippingAddress } from '@/types';
-import { auth } from '@/auth';
+import { getOrderById } from '@/lib/actions/order.actions'
+import { Metadata } from 'next'
+import { notFound } from 'next/navigation'
+import OrderDetailsTable from './order-details-table'
+import { ShippingAddress } from '@/types'
+import { auth } from '@/auth'
 
 
 export const metadata: Metadata = {
-  title: 'Order Details',
-};
+	title: 'Order Details',
+}
 
-const OrderDetailsPage = async ({
-  params,
-}: {
-  params: {
-    id: string;
-  };
-}) => {
-  const { id } = params;
+type Props = {
+	params: Promise<{ id: string }>
+}
+const OrderPage = async ({ params }: Props) => {
+	const { id } = await params
 
-  const order = await getOrderById(id);
-  if (!order) notFound();
+	const order = await getOrderById(id)
 
-  const session = await auth();
+	if (!order) notFound()
 
-    console.log(order)
+	const session = await auth()
 
-  // If user is not the owner or not admin, redirect
-  if (order.userId !== session?.user?.id && session?.user?.role !== 'admin') {
-    redirect('/unauthorized');
-  }
+	// let client_secret = null
 
-  return (
-    <OrderDetailsTable
-      order={{
-        ...order,
-        shippingAddress: order.shippingAddress as ShippingAddress,
-      }}
-      paypalClientId={process.env.PAYPAL_CLIENT_ID || 'sb'}
-      isAdmin={session?.user?.role === 'admin' || false}
-    />
-  );
-};
+	// // Check if is not paid and using stripe
+	// if (order.paymentMethod === 'Stripe' && !order.isPaid) {
+	// 	// Init stripe instance
+	// 	const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
+	// 	// Create payment intent
+	// 	const paymentIntent = await stripe.paymentIntents.create({
+	// 		amount: Math.round(Number(order.totalPrice) * 100),
+	// 		currency: 'USD',
+	// 		metadata: { orderId: order.id },
+	// 	})
+	// 	client_secret = paymentIntent.client_secret
+	// }
 
-export default OrderDetailsPage;
+	return (
+		<OrderDetailsTable
+			order={{
+				...order,
+				shippingAddress: order.shippingAddress as ShippingAddress,
+			}}
+			paypalClientId={process.env.PAYPAL_CLIENT_ID || 'sb'}
+			isAdmin={session?.user?.role === 'admin' || false}
+		/>
+	)
+}
+export default OrderPage
